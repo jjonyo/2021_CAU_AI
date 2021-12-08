c7c4051490565e4b2fca8bd55dd650a826f10ced
jjonyo <jonghyuk96@cau.ac.kr>
2021-12-08 17:25:35 +0900

    Week13 assignment

---
+++ w13_TenserflowKerasBased1.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+# 가상적인 데이터 생성
+X = data = np.linspace(1, 2, 200) # 시작값=1, 종료값=2, 개수=200
+y = X*4 + np.random.randn(200)*0.3 # X를 4배로 하고 편차 0.3 정도의 가우시안잡음 추가
+
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Dense(1, input_dim=1, activation='linear'))
+model.compile(optimizer='sgd', loss='mse', metrics=['mse'])
+model.fit(X, y, batch_size=1, epochs=100)
+
+predict = model.predict(data)
+
+plt.plot(data, predict, 'b', data, y, 'k. ') # 첫 번째 그래프는 파란색 마커로
+plt.show() # 두 번째 그래프는 검정색 .으로 그림
